585d9c21fa48aaf4fe59376c57d334df90a6b065
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-03 10:31:03 +0530

    made connection to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://admin:aKSHw2njjioupAYz@cluster0.bxkhk0r.mongodb.net/")
+  .then(() => console.log("connected to MongoDB...."))
+  .catch((err) => console.log("Couldn't connect to MongoDB!!"));
 
 app.use(express.json());
