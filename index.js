010f62e2f6418ca52be8dadd75d190322e74f274
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-03 12:32:20 +0530

    created user login route with email and password validation

--- index.js
+++ index.js
@@ -54,6 +54,40 @@ app.post("/registration", async (req, res) => {
   });
 });
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const jwtToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({
+      message: "User logged in successfully",
+      token: jwtToken,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+});
+
 app.post("/admin/registration", async (req, res) => {
   const validation = registrationSchema.safeParse(req.body);
   if (!validation.success) {
