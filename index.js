bb3422005845d0cb4c100b148ed29f3f80aa3549
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-03 11:34:15 +0530

    created admin registration api

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const connectDB = require("./db");
+const bcrypt = require("bcrypt");
+const Admin = require("./models/admin.models");
+const User = require("./models/user.model");
 
 connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/admin/registration", async (req, res) => {
+  const { username, password, email } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const exisitingAdmin = await Admin.findOne({ email });
+
+  if (exisitingAdmin) {
+    return res.json({
+      message: "Admin already exists",
+    });
+  }
+
+  let admin = new Admin({
+    username,
+    password: hashedPassword,
+    email,
+  });
+
+  admin = await admin.save();
+
+  res.json({
+    msg: "Admin created successfully",
+    admin,
+  });
 });
 
 app.listen(process.env.PORT, () => {
