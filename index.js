c6ab6d73594aba481ad042b613be7f3ad1628978
Sanskar Chirania <sanskar125112@gmail.com>
2024-10-03 11:21:30 +0530

    created admin and user model

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const connectDB = require("./db");
 
-mongoose
-  .connect("mongodb+srv://admin:aKSHw2njjioupAYz@cluster0.bxkhk0r.mongodb.net/")
-  .then(() => console.log("connected to MongoDB...."))
-  .catch((err) => console.log("Couldn't connect to MongoDB!!"));
+connectDB();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+// user.model.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
